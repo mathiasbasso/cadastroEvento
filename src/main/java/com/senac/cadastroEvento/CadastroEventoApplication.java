a4bc1f147561f6d0a299b6050aadefd3ca8e7c8c
mathiasbasso <mathias201711@gmail.com>
2023-10-16 19:45:39 -0300

    Initial commit

---
+++ src/main/java/com/senac/cadastroEvento/CadastroEventoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.cadastroEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/cadastroEvento/CadastroEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.cadastroEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
